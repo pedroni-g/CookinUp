866958ff62d71a73ae4be5b2b7207059538e8765
Gabriel Pedroni <sleepy.pepperoni@gmail.com>
2025-06-20 13:53:05 -0300

    feat - first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import Banner from "./components/Banner.vue";
+
+export default {
+	// components: { Banner: Banner }
+	components: { Banner }
+}
+
+</script>
+
+<template>
+	<Banner />
+</template>
